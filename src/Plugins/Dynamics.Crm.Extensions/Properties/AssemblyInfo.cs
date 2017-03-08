d4548dcf3a7e4c30412995a4941f409771ccff48
Sergio Escalera <sergio.escalera.c@gmail.com>
2017-03-08 13:11:11 -0500

    Added Dynamics.Crm.Extensions class library.

---
+++ src/Plugins/Dynamics.Crm.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Dynamics.Crm.Extensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dynamics.Crm.Extensions")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b8f74789-d7e3-4ddb-be1a-13a8c44db72f")]
+
+[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
