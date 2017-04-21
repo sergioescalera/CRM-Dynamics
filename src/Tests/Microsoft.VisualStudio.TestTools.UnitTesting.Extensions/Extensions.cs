7d923c2d0a40009b7027779463008c1234d8d89c
Sergio Escalera <sergio.escalera.c@gmail.com>
2017-04-21 17:14:05 -0400

    Microsoft.VisualStudio.TestTools.UnitTesting.Extensions initial check in.

---
+++ src/Tests/Microsoft.VisualStudio.TestTools.UnitTesting.Extensions/Extensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
+{
+    public static class Extensions
+    {
+        static readonly Func<String, String> noExceptionMessage = (methodName) =>
+            $"Test method {methodName} did not throw an exception. An exception was expected on the test method.";
+
+        static readonly Func<String, Type, Type, String, String> invalidExceptionTypeMessage = (methodName, expectedType, actualType, exceptionMessage) =>
+            $"Test method {methodName} threw exception {actualType.FullName}, but exception {expectedType.FullName} was expected. Exception message: {exceptionMessage}";
+
+        static readonly Func<String, String, String, String> invalidExceptionMessageMessage = (methodName, expectedMessage, actualMessage) =>
+            $"Test method {methodName} threw exception with message '{actualMessage}', but message '{expectedMessage}' was expected.";
+
+        public static void ExpectedException<TException>(
+            this Object test,
+            Action action,
+            Boolean allowDerivedTypes = false,
+            String expectedMessage = null)
+            where TException : Exception
+        {
+            try
+            {
+                action();                
+            }
+            catch (TException ex)
+            {
+                var actualType = ex.GetType();
+                var expectedType = typeof(TException);
+
+                Assert.IsTrue(
+                    allowDerivedTypes || actualType.Equals(expectedType),
+                    invalidExceptionTypeMessage(
+                        action.Method.Name,
+                        expectedType,
+                        actualType,
+                        ex.Message));
+
+                Assert.IsTrue(String.IsNullOrEmpty(expectedMessage) || String.Equals(ex.Message, expectedMessage),
+                    invalidExceptionMessageMessage(
+                        action.Method.Name,
+                        expectedMessage,
+                        ex.Message));
+
+                return;
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(
+                    invalidExceptionTypeMessage(
+                        action.Method.Name,
+                        typeof(TException),
+                        ex.GetType(),
+                        ex.Message));
+
+                return;
+            }
+
+            Assert.Fail(noExceptionMessage(action.Method.Name));
+        }
+    }
+}
---
+++ src/Tests/Microsoft.VisualStudio.TestTools.UnitTesting.Extensions/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Microsoft.VisualStudio.TestTools.UnitTesting.Extensions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Microsoft.VisualStudio.TestTools.UnitTesting.Extensions")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e68fd44d-8cf1-4a56-b5bd-95b62d8cf081")]
+
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
