9df82094253aea5aa4339a6e5275bea5c771a3bd
Sergio Escalera <sergio.escalera.c@gmail.com>
2017-07-21 09:47:51 -0400

    Added Assembly extensions.

---
+++ src/Common/System.Extensions.Shared/Reflection/AssemblyExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Text;
+
+namespace System.Reflection
+{
+    public static class AssemblyExtensions
+    {
+        public static Stream GetResourceStream(this Object obj, String resourceName)
+        {
+            ValidationHelper.EnsureNotNull(obj, nameof(obj));
+            ValidationHelper.EnsureNotNull(resourceName, nameof(resourceName));
+
+            var type = obj.GetType();
+            var assembly = type.Assembly;
+
+            return assembly.GetManifestResourceStream(resourceName);
+        }
+
+        public static StreamReader GetResourceReader(this Object obj, String resourceName)
+        {
+            return GetResourceReader(obj, resourceName, Encoding.UTF8);
+        }
+
+        public static StreamReader GetResourceReader(
+            this Object obj,
+            String resourceName,
+            Encoding encoding)
+        {
+            ValidationHelper.EnsureNotNull(obj, nameof(obj));
+            ValidationHelper.EnsureNotNull(resourceName, nameof(resourceName));
+
+            var type = obj.GetType();
+            var assembly = type.Assembly;
+
+            var stream = assembly.GetManifestResourceStream(resourceName);
+
+            return new StreamReader(stream, encoding);
+        }
+    }
+}
---
+++ src/Tests/System.Extensions.UnitTests/AssemblyFacts.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace System.Extensions.UnitTests
+{
+    public class AssemblyFacts
+    {
+        [TestClass]
+        public class GetResourceReaderMethod
+        {
+            [TestMethod]
+            public async Task ShouldOpenEmbeddedResource()
+            {
+                var reader = this.GetResourceReader("System.Extensions.UnitTests.Resources.SampleFile.txt");
+
+                Assert.IsNotNull(reader);
+
+                var text = await reader.ReadToEndAsync();
+
+                Assert.IsNotNull(text);
+                Assert.AreEqual("This is a sample text file used for unit testing", text);
+            }
+        }
+    }
+}
