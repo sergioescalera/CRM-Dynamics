47b6fac65f9e2f9e146dd8ce52adf3efc12535b0
Sergio Escalera <sergio.escalera.c@gmail.com>
2017-08-22 09:12:09 -0400

    Added HasValue extension method for objects.

--- src/Common/System.Extensions.Shared/Extensions/ObjectExtensions.cs
+++ src/Common/System.Extensions.Shared/Extensions/ObjectExtensions.cs
@@ -2,6 +2,11 @@
 {
     public static class ObjectExtensions
     {
+        public static Boolean HasValue(this Object value)
+        {
+            return value != null;
+        }
+
         public static Boolean IsNumeric(this Object value)
         {
             return value is sbyte
