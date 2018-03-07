8f8f87dca5fad610257e01caae08a4a352d28870
Sergio Escalera <sergio.escalera.c@gmail.com>
2018-03-07 08:43:08 -0500

    Added string concat method.

--- src/Common/System.Extensions.Shared/Linq/EnumerableExtensions.cs
+++ src/Common/System.Extensions.Shared/Linq/EnumerableExtensions.cs
@@ -4,8 +4,8 @@
     {
         public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
         {
-            ValidationHelper.EnsureNotNull(source);
-            ValidationHelper.EnsureNotNull(action);
+            ValidationHelper.EnsureNotNull(source, nameof(source));
+            ValidationHelper.EnsureNotNull(action, nameof(action));
 
             foreach (var item in source)
             {
@@ -15,10 +15,17 @@
 
         public static void AddRange<T>(this ICollection<T> source, IEnumerable<T> collection)
         {
-            ValidationHelper.EnsureNotNull(source);
-            ValidationHelper.EnsureNotNull(collection);
+            ValidationHelper.EnsureNotNull(source, nameof(source));
+            ValidationHelper.EnsureNotNull(collection, nameof(collection));
 
             collection.ForEach(o => source.Add(o));
+        }
+
+        public static String Concatenate<T>(this IEnumerable<T> source, String separator)
+        {
+            ValidationHelper.EnsureNotNull(source, nameof(source));
+
+            return String.Join(separator, source);
         }
     }
 }
