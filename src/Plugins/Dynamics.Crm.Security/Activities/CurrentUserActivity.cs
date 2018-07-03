7603d0cae45e12b2c4560bafc98744b3c4364815
Sergio Escalera <sergio.escalera.c@gmail.com>
2018-07-03 16:54:49 -0400

    Get current user activity.

---
+++ src/Plugins/Dynamics.Crm.Security/Activities/CurrentUserActivity.cs
@@ -0,0 +1,25 @@
+﻿using Dynamics.Crm.Activities;
+using Dynamics.Crm.Extensions;
+using Dynamics.Crm.Interfaces;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Workflow;
+using System.Activities;
+
+namespace Dynamics.Crm.Security.Activities
+{
+    public class CurrentUserActivity : WorkflowActivityBase
+    {
+        [Output("User")]
+        [ReferenceTarget("systemuser")]
+        public OutArgument<EntityReference> User { get; set; }
+
+        protected override void Execute(IWorkflowActivityContext context)
+        {
+            var executionContext = context.GetExecutionContext();
+
+            var user = new EntityReference("systemuser", context.WorkflowContext.InitiatingUserId);
+
+            User.Set(executionContext, user);
+        }
+    }
+}
